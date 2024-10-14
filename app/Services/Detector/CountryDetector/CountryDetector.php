ab588811b696b899d4972de5f614f1d5bf14d947
iliya <lazarev.rollun@gmail.com>
2024-10-14 22:41:02 +0300

    feat: CountryDetector

--- app/Services/Detector/CountryDetector/CountryDetector.php
+++ app/Services/Detector/CountryDetector/CountryDetector.php
@@ -4,6 +4,7 @@ namespace App\Services\Detector\CountryDetector;
 
 use App\Common\DTO\Country;
 use App\Common\DTO\Ip;
+use App\Exceptions\NoIsoCodeCountryException;
 use GeoIp2\Exception\AddressNotFoundException;
 use GeoIp2\ProviderInterface;
 
@@ -13,11 +14,17 @@ class CountryDetector implements CountryDetectorInterface
     {
     }
 
+    /**
+     * @throws \App\Exceptions\NoIsoCodeCountryException
+     */
     public function detect(Ip $ip): Country
     {
         try {
             $country = $this->countryDetector->country($ip->getValue());
             $isoCode = $country->country->isoCode;
+            if (null === $isoCode) {
+                throw new NoIsoCodeCountryException('No iso for ip ' . $ip->getValue());
+            }
             return new Country($isoCode);
         } catch (AddressNotFoundException $exception) {
             throw new \InvalidArgumentException("Country for ip '{$ip->getValue()}' does not exist.");
